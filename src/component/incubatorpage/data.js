0946bfe91bed36a90017e00eb3dd49fd4b1997f8
arthur orduh <arthurorduh@gmail.com>
2021-08-25 15:16:09 +0100

    update

--- src/component/incubatorpage/data.js
+++ src/component/incubatorpage/data.js
@@ -28,5 +28,14 @@ const menu = [
         percent: 15.99,
         imag: imag
     },
+    {
+        id: 4,
+        title: 'Mangoswap',
+        numerator:  '200,000',
+        denominator: '400,000',
+        des: 'The mango swap coin is the future for crypto currency, it will allow user share and tansact over defi networks with less fees.',
+        percent: 15.99,
+        imag: imag
+    },
 ]
 export default menu;
